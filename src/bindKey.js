07a77d0483d0c5f5dada28a8ebb6378ba6dab7c8
ZhenhuiSu <38357535+ZhenhuiSu@users.noreply.github.com>
2021-04-15 11:52:34 +0800

    fix:修复ctrl+alt+B也能触发弹窗问题
    fix:修复ctrl+alt+B也能触发弹窗问题
    format:修改为tampermonkey推荐的es5格式

--- src/bindKey.js
+++ src/bindKey.js
@@ -1,39 +1,62 @@
 // ==UserScript==
 // @name         gitMind-bindKey
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      1.1
 // @description  gitMind自定义绑定快捷键，插入链接(alt+L)，插入备注(alt+B)，退出弹出窗(esc)
 // @author       zhenhuiSu
 // @match        https://gitmind.cn/app/doc/*
 // @grant        none
 // @require https://cdn.jsdelivr.net/npm/jquery@1.5.1/dist/node-jquery.min.js
 // ==/UserScript==
+(function () {
+    bindHotKey();
 
-bindHotKey();
-
-function bindHotKey(){
-    document.onkeydown = function(){
-        // gitMind内置快捷键绑定的窗口，退出弹出窗后需要使该组件获取焦点，否则内置快捷键将不可用
-        var qlEditor = $('.ql-editor');
-        var key = window.event.keyCode;
-        if (event.altKey) {
-            switch(key) {
-                case 66:
-                    $('.icon-beizhu').click();
-                    break;
-                case 76:
-                    $('.icon-link').click();
-                    break;
+    // ctrl shift alt key
+    var bindMap = {
+        // 00027 ESC
+        27: function () {
+            var qlEditor = $('.ql-editor');
+            var remarkPop = $('.ne-title');
+            if (remarkPop) {
+                remarkPop.children('.icon-guanbi').click()
             }
-        } else {
-            switch(key) {
-                case 27:
-                    if ($('.ne-title')) {
-                        $('.ne-title').children('.icon-guanbi').click();
-                    }
-                    qlEditor.focus();
-                    break;
+
+            if (qlEditor) {
+                qlEditor.focus();
+            }
+        },
+        // 00166 alt B
+        166: function () {
+            var remarkEl = $('.icon-beizhu');
+            if (remarkEl) {
+                remarkEl.click();
+            }
+        },
+        // 00176 alt L
+        176: function () {
+            var remarkEl = $('.icon-beizhu');
+            if (remarkEl) {
+                remarkEl.click();
             }
         }
-    };
-}
+    }
+
+    function bindHotKey() {
+        document.onkeydown = function () {
+            // gitMind内置快捷键绑定的窗口，退出弹出窗后需要使该组件获取焦点，否则内置快捷键将不可用
+            var fun = choiceFun(window.event);
+            if (fun) {
+                fun.apply();
+            }
+        };
+    }
+
+    function choiceFun(e) {
+        var key = 0;
+        key = key + e.ctrlKey;
+        key = (key * 10) + e.shiftKey;
+        key = (key * 10) + e.altKey;
+        key = (key * 100) + e.keyCode;
+        return bindMap[key];
+    }
+})();
